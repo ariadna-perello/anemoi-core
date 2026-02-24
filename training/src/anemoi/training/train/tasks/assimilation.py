0d2c4b4492fcd3616b88b6800904c51257b069d6
Ariadna Perello Achfari <fra6955@ac6-102.bullx>
2026-02-24 15:26:09 +0000

    classe GraphAnalysisToAnalysis non testée

---
+++ training/src/anemoi/training/train/tasks/assimilation.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+import logging
+#from typing import TYPE_CHECKING
+
+from torch.utils.checkpoint import checkpoint
+
+from anemoi.training.train.tasks.base import BaseGraphModule
+
+#if TYPE_CHECKING:
+from collections.abc import Mapping
+
+import torch
+from omegaconf import DictConfig
+from torch_geometric.data import HeteroData
+
+from anemoi.models.data_indices.collection import IndexCollection
+
+
+LOGGER = logging.getLogger(__name__)
+
+class GraphAnalysisToAnalysis(BaseGraphModule): 
+    task_type = "assimilation"
+
+    def __init__(
+        self,
+        *,
+        config: DictConfig,
+        graph_data: HeteroData,
+        statistics: dict,
+        statistics_tendencies: dict,
+        data_indices: IndexCollection,
+        metadata: dict,
+        supporting_arrays: dict,
+    ) -> None:
+        """Initialize graph neural network assimilator.
+
+        Parameters
+        ----------
+        config : DictConfig
+            Job configuration
+        graph_data : HeteroData
+            Graph object
+        statistics : dict
+            Statistics of the training data
+        data_indices : IndexCollection
+            Indices of the training data,
+        metadata : dict
+            Provenance information
+        supporting_arrays : dict
+            Supporting NumPy arrays to store in the checkpoint
+
+        """
+        super().__init__(
+            config=config,
+            graph_data=graph_data,
+            statistics=statistics,
+            statistics_tendencies=statistics_tendencies,
+            data_indices=data_indices,
+            metadata=metadata,
+            supporting_arrays=supporting_arrays,
+        )
+
+    def _step(
+        self,
+        batch: dict[str, torch.Tensor],
+        validation_mode: bool = False,
+    ) -> tuple[torch.Tensor, Mapping[str, torch.Tensor]]:
+
+        x = {}
+
+        for dataset_name, dataset_batch in batch.items():
+            x[dataset_name] = dataset_batch[
+                :,
+                0,
+                ...,
+                self.data_indices[dataset_name].data.input.full,
+            ]
+
+        y_pred = self(x)
+
+        y = {}
+
+        for dataset_name, dataset_batch in batch.items():
+            y[dataset_name] = dataset_batch[
+                :,
+                0,
+                ...,
+                self.data_indices[dataset_name].data.output.full,
+            ]
+
+        loss, metrics, y_pred = checkpoint(
+            self.compute_loss_metrics,
+            y_pred,
+            y,
+            rollout_step=0,
+            training_mode=True,
+            validation_mode=validation_mode,
+            use_reentrant=False,
+        )
+
+        return loss, metrics, y_pred
+
+    def on_train_epoch_end(self) -> None:
+        pass
